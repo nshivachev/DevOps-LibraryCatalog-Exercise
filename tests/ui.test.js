6537593df89e9a7c91bb070d50775d3ff70d539a
nshivachev <shivachev.nikolay@gmail.com>
2024-06-11 06:47:25 +0300

    Updated yml file

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -1,21 +1,21 @@
 const {test, expect} = require("@playwright/test");
 const url = "http://localhost:3000";
 
-// test("Verify All Books link is visible", async ( {page} ) => {
-//     await page.goto(url);
-//     await page.waitForSelector('nav.navbar');
-//     const allBooksLink = await page.$('a[href="/catalog"]');
-//     const isLinkVisible = await allBooksLink.isVisible();
-//     expect(isLinkVisible).toBe(true);
-// });
-
-test("Verify Login button is visible", async ( {page} ) => {
+test("Verify All Books link is visible", async ( {page} ) => {
     await page.goto(url);
     await page.waitForSelector('nav.navbar');
-    const loginButton = await page.$('a[href="/login"]');
-    const isLinkVisible = await loginButton.isVisible();
+    const allBooksLink = await page.$('a[href="/catalog"]');
+    const isLinkVisible = await allBooksLink.isVisible();
     expect(isLinkVisible).toBe(true);
 });
+
+// test("Verify Login button is visible", async ( {page} ) => {
+//     await page.goto(url);
+//     await page.waitForSelector('nav.navbar');
+//     const loginButton = await page.$('a[href="/login"]');
+//     const isLinkVisible = await loginButton.isVisible();
+//     expect(isLinkVisible).toBe(true);
+// });
 
 // test("Verify Register button is visible", async ( {page} ) => {
 //     await page.goto(url);
